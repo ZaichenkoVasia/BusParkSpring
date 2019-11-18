0ee262369a588513589d6f5e63173a885f1eddd4
unknown <zaichenko.vasyl@gmail.com>
2019-11-18 19:17:55 +0200

    Add contract for bus and route service

--- src/main/java/ua/mycompany/buspark/model/service/BusService.java
+++ src/main/java/ua/mycompany/buspark/model/service/BusService.java
@@ -1,4 +1,17 @@
 package ua.mycompany.buspark.model.service;
 
+import ua.mycompany.buspark.model.domain.Bus;
+import ua.mycompany.buspark.model.domain.User;
+import ua.mycompany.buspark.model.domain.enums.Status;
+
+import java.util.List;
+
 public interface BusService {
+    List<Bus> findAll(Integer currentPage, Integer recordsPerPage);
+
+    long showNumberOfRows();
+
+    Bus findByUser(User user);
+
+    List<Bus> findByStatus(Status status);
 }
--- src/main/java/ua/mycompany/buspark/model/service/RouteService.java
+++ src/main/java/ua/mycompany/buspark/model/service/RouteService.java
@@ -1,4 +1,21 @@
 package ua.mycompany.buspark.model.service;
 
+import ua.mycompany.buspark.model.domain.Bus;
+import ua.mycompany.buspark.model.domain.Route;
+import ua.mycompany.buspark.model.domain.enums.Status;
+
+import java.util.List;
+
 public interface RouteService {
+    List<Route> findAll(Integer currentPage, Integer recordsPerPage);
+
+    long showNumberOfRows();
+
+    Route findByBus(Bus bus);
+
+    List<Route> findByStatus(Status status);
+
+    List<Route> findByArrival(String arrival);
+
+    List<Route> findByDeparture(String departure);
 }
