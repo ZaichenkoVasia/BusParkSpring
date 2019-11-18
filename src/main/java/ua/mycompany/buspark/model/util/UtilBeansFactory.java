0b634b12fc5ee90217e44c6c577dc75edae4b6b6
unknown <zaichenko.vasyl@gmail.com>
2019-11-18 19:38:13 +0200

    Fix encoder

---
+++ src/main/java/ua/mycompany/buspark/model/util/UtilBeansFactory.java
@@ -0,0 +1,13 @@
+package ua.mycompany.buspark.model.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UtilBeansFactory {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
