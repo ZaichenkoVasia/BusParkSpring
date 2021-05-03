7a23cc28961aeb62d20176fd9ba94d0bd31a543d
Zaichenko <zaichenko.vasyl@gmail.com>
2021-05-03 18:19:55 +0300

    Add identifier

--- src/main/java/com/transportpark/controller/BusController.java
+++ src/main/java/com/transportpark/controller/BusController.java
@@ -2,6 +2,7 @@ package com.transportpark.controller;
 
 import com.transportpark.model.domain.Bus;
 import com.transportpark.model.service.BusService;
+import com.transportpark.model.service.FileService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,11 +19,12 @@ import java.util.stream.IntStream;
 @Controller
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BusController {
-
+    private final FileService fileService;
     private final BusService busService;
 
     @GetMapping("/bus")
-    public String viewBuses(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
+    public String viewBuses(Model model, @RequestParam("page") Optional<Integer> page,
+                            @RequestParam("size") Optional<Integer> size) {
         addPagination(model, page, size);
         model.addAttribute("bus", new Bus());
         return "/bus";
@@ -31,14 +33,19 @@ public class BusController {
     @PostMapping("/bus")
     public String addBus(Model model, @ModelAttribute Bus bus,
                          @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
-        busService.addBus(bus);
-        model.addAttribute("addedBus", bus.getModel());
+        Boolean identify = fileService.getIdentify();
+        if (identify) {
+            busService.addBus(bus);
+            model.addAttribute("addedBus", bus.getModel());
+        }
+        model.addAttribute("identify", identify.toString());
         addPagination(model, page, size);
         return "/bus";
     }
 
     @GetMapping("/bus/edit/{code}")
-    public String editBus(Model model, @PathVariable Integer code, @RequestParam("page") Optional<Integer> page,
+    public String editBus(Model model, @PathVariable Integer code,
+                          @RequestParam("page") Optional<Integer> page,
                           @RequestParam("size") Optional<Integer> size) {
         model.addAttribute("editCode", code);
         addPagination(model, page, size);
@@ -47,9 +54,11 @@ public class BusController {
 
     @PostMapping("/bus/edit/{code}")
     public ModelAndView updateBus(Model model, @PathVariable Integer code,
-                                  @RequestParam("changeMileage") Double changeMileage, @RequestParam("changeConsumption") Double changeConsumption,
+                                  @RequestParam("changeMileage") Double changeMileage,
+                                  @RequestParam("changeConsumption") Double changeConsumption,
                                   @RequestParam("changeDriver") Long idDriver,
-                                  @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
+                                  @RequestParam("page") Optional<Integer> page,
+                                  @RequestParam("size") Optional<Integer> size) {
         busService.changeBus(code, changeMileage, changeConsumption, idDriver);
         addPagination(model, page, size);
         return new ModelAndView("redirect:/bus");
